b66aa62a3b2e3ac00091c5282db5820f5fb17caa
David Villa Alises <David.Villa@gmail.com>
2016-10-25 14:40:30 +0100

    first release

---
+++ scone_client.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+# -*- coding: utf-8; mode: python -*-
+
+import socket
+
+BUFFERSIZE = 8192
+ENCODER = 'utf-8'
+PROMPT = b'[PROMPT]'
+SCONE_ERROR = '*****SCONE-ERROR*****'
+
+
+class SconeError(Exception):
+    pass
+
+
+class SconeClient:
+    def __init__(self, host='localhost', port=6517):
+        self.sock = socket.socket()
+        self.sock.connect((host, port))
+        self.buffer = b''
+
+    def get_line(self):
+        def next_line(where):
+            line, self.buffer = where.split(b'\n', 1)
+            return line
+
+        if b'\n' in self.buffer:
+            return next_line(self.buffer)
+
+        data = bytes()
+        while b'\n' not in data:
+            data += self.sock.recv(BUFFERSIZE)
+
+        line = next_line(data)
+        # print("get-line rest:", self.buffer)
+        return line
+
+    def send(self, sentence):
+        assert self.get_line() == PROMPT
+        sentence = sentence.strip() + '\n'
+        self.sock.sendall(sentence.encode(ENCODER))
+        response = self.get_line().decode(ENCODER)
+        self.check_error(response)
+        return response
+
+    def predicate(self, sentence):
+        response = self.send(sentence)
+
+        assert response in ['YES', 'NO', 'MAYBE']
+        return response
+
+    def query(self, sentence):
+        return self.send(sentence)
+
+    def check_error(self, response):
+        if response.startswith(SCONE_ERROR):
+            assert self.get_line() == b'NIL'
+            msg = response.split('Error:')[1]
+            raise SconeError(msg)
---
+++ test/integration_tests.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8; mode: python -*-
+
+from unittest import TestCase
+from scone_client import SconeClient, SconeError
+
+from doublex import assert_that, is_
+
+
+class ClientTests(TestCase):
+    @classmethod
+    def setupClass(self):
+        SconeClient().query('(new-indv {checkpoint} {thing})')
+
+    @classmethod
+    def tearDownClass(self):
+        SconeClient().query('(remove-elements-after {checkpoint})')
+        SconeClient().query('(remove-last-element)')
+
+    def setUp(self):
+        self.sut = SconeClient()
+
+    def test_predicate_yes(self):
+        self.sut.predicate('(is-x-a-y? {bird} {animal})')
+
+    def test_predicate_redundant_blanks(self):
+        response = self.sut.predicate('\n\n\n(is-x-a-y? {bird} {animal})\n\n\n')
+        assert_that(response, is_('YES'))
+
+    def test_predicate_no(self):
+        response = self.sut.predicate('(is-x-a-y? {bird} {mammal})')
+        assert_that(response, is_('NO'))
+
+    def test_predicate_maybe(self):
+        response = self.sut.predicate('(is-x-a-y? {broom} {air transport})')
+        assert_that(response, is_('MAYBE'))
+
+    def test_predicate_error(self):
+        expected_msg = 'The function COMMON-LISP-USER::MISSING-FUNCTION is undefined.'
+
+        try:
+            self.sut.predicate('(missing-function)')
+            self.fail('exception should be raised!')
+        except SconeError as e:
+            assert_that(str(e), is_(expected_msg))
+
+    def test_new_indv(self):
+        response = self.sut.query('(new-indv {Marta} {elephant})')
+        assert_that(response, is_('{Marta}'))
+
+    def test_new_is_a_OK(self):
+        response = self.sut.query('(new-indv {Carlos} {thing})')
+        assert_that(response, is_('{Carlos}'))
+
+        response = self.sut.query('(new-is-a {Carlos} {bird})')
+        assert_that(response[:8], is_('{Is-A 0-'))
+
+    def test_new_is_a_FAIL(self):
+        response = self.sut.query('(new-indv {Daniel} {elephant})')
+        assert_that(response, is_('{Daniel}'))
+
+        expected_msg = '{Daniel} cannot be a {bird}.'
+
+        try:
+            response = self.sut.query('(new-is-a {Daniel} {bird})')
+            self.fail('exception should be raised!')
+        except SconeError as e:
+            assert_that(str(e), is_(expected_msg))
